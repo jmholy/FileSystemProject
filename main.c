09d1c33ab0c933bef9ae03c5fb28258916016bdd
jmholy <jemarho@msn.com>
2016-04-05 17:11:58 -0700

    Added Function Basics

--- main.c
+++ main.c
@@ -29,3 +29,154 @@ char buf[BLKSIZE];
 char *pathitems[32];
 char *path;
 char *dev;
+
+//Level 1
+void mount_root()
+{
+
+}
+void mkdir()
+{
+
+}
+void rmdir()
+{
+
+}
+
+void ls()
+{
+
+}
+void cd()
+{
+
+}
+void pwd()
+{
+
+}
+void creat()
+{
+
+}
+void link()
+{
+
+}
+void unlink()
+{
+
+}
+void symlink()
+{
+
+}
+void stat()
+{
+
+}
+void chmod()
+{
+
+}
+void touch()
+{
+
+}
+void quit()
+{
+
+}
+void chown(newOwner, pathname)
+{
+
+}
+void chgrp(newGroup, pathname)
+{
+
+}
+//Level 2
+void open()
+{
+
+}
+void close()
+{
+
+}
+void read()
+{
+
+}
+void write()
+{
+
+}
+void lseek()
+{
+
+}
+void cat()
+{
+
+}
+void cp()
+{
+
+}
+void mv()
+{
+
+}
+//Level 3
+void mount()
+{
+
+}
+void unmount()
+{
+
+}
+int checkPerm()
+{
+
+}
+//Helper Functions
+int get_block(int dev, int blk, char *buf)
+{
+
+}
+int put_block(int dev, int blk, char *buf)
+{
+
+}
+int ialloc(int dev)        // allocate an ino
+{
+
+}
+int balloc(int dev)        // allocate a  bno
+{
+
+}
+int idealloc(int dev, int ino) // deallocate ino
+{
+
+}
+int bdealloc(int dev, int bno) // deallocate bno
+{
+
+}
+int enter_name(MINODE *pip, int myino, char *myname)
+{
+
+}
+int rmchild(MINODE *parent, char *name)
+{
+
+}
+//main
+int main(int agrc, char *agrv[], char *env[])
+{
+
+}
